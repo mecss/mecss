e9bbcf5e65a8788002c2b0a76d84f40da1b835b9
blyndusk <alexandredelaloydev@gmail.com>
2019-04-29 20:20:33 +0200

    🗃 add border data

--- lib/Data.ts
+++ lib/Data.ts
@@ -104,6 +104,234 @@ const Data = {
             in: `bd:|bd +:`,
             out: `border:`,
         },
+        {
+            in: `bd-bl:|bd-bl +:`,
+            out: `border-block:`,
+        },
+        {
+            in: `bd-bl-c:|bd-bl-c +:`,
+            out: `border-block-color:`,
+        },
+        {
+            in: `bd-bl-e:|bd-bl-e +:`,
+            out: `border-block-end:`,
+        },
+        {
+            in: `bd-bl-e-c:|bd-bl-e-c +:`,
+            out: `border-block-end-color:`,
+        },
+        {
+            in: `bd-bl-e-s:|bd-bl-e-s +:`,
+            out: `border-block-end-style:`,
+        },
+        {
+            in: `bd-bl-e-w:|bd-bl-e-w +:`,
+            out: `border-block-end-width:`,
+        },
+        {
+            in: `bd-bl-st:|bd-bl-st +:`,
+            out: `border-block-start:`,
+        },
+        {
+            in: `bd-bl-st-c:|bd-bl-st-c +:`,
+            out: `border-block-start-color:`,
+        },
+        {
+            in: `bd-bl-st-sty:|bd-bl-st-sty +:`,
+            out: `border-block-start-style:`,
+        },
+        {
+            in: `bd-bl-st-w:|bd-bl-st-w +:`,
+            out: `border-block-start-width:`,
+        },
+        {
+            in: `bd-bl-sty:|bd-bl-sty +:`,
+            out: `border-block-style:`,
+        },
+        {
+            in: `bd-bl-w:|bd-bl-w +:`,
+            out: `border-block-width:`,
+        },
+        {
+            in: `bd-bt:|bd-bt +:`,
+            out: `border-bottom:`,
+        },
+        {
+            in: `bd-bt-c:|bd-bt-c +:`,
+            out: `border-bottom-color:`,
+        },
+        {
+            in: `bd-bt-l-rd:|bd-bt-l-rd +:`,
+            out: `border-bottom-left-radius:`,
+        },
+        {
+            in: `bd-bt-r-rd:|bd-bt-r-rd +:`,
+            out: `border-bottom-right-radius:`,
+        },
+        {
+            in: `bd-clp:|bd-clp +:`,
+            out: `border-collapse:`,
+        },
+        {
+            in: `bd-e-e-rd:|bd-e-e-rd +:`,
+            out: `border-end-end-radius:`,
+        },
+        {
+            in: `bd-e-s-rd:|bd-e-s-rd +:`,
+            out: `border-end-start-radius:`,
+        },
+        {
+            in: `bd-img:|bd-img +:`,
+            out: `border-image:`,
+        },
+        {
+            in: `bd-img-o:|bd-img-o +:`,
+            out: `border-image-outset:`,
+        },
+        {
+            in: `bd-img-r:|bd-img-r +:`,
+            out: `border-image-repeat:`,
+        },
+        {
+            in: `bd-img-sl:|bd-img-sl +:`,
+            out: `border-image-slice:`,
+        },
+        {
+            in: `bd-img-sr:|bd-img-sr +:`,
+            out: `border-image-source:`,
+        },
+        {
+            in: `bd-img-w:|bd-img-w +:`,
+            out: `border-image-width:`,
+        },
+        {
+            in: `bd-i:|bd-i +:`,
+            out: `border-inline:`,
+        },
+        {
+            in: `bd-i-c:|bd-i-c +:`,
+            out: `border-inline-color:`,
+        },
+        {
+            in: `bd-i-e:|bd-i-e +:`,
+            out: `border-inline-end:`,
+        },
+        {
+            in: `bd-i-e-c:|bd-i-e-c +:`,
+            out: `border-inline-end-color:`,
+        },
+        {
+            in: `bd-i-e-s:|bd-i-e-s +:`,
+            out: `border-inline-end-style:`,
+        },
+        {
+            in: `bd-i-e-w:|bd-i-e-w +:`,
+            out: `border-inline-end-width:`,
+        },
+        {
+            in: `bd-i-st:|bd-i-st +:`,
+            out: `border-inline-start:`,
+        },
+        {
+            in: `bd-i-st-c:|bd-i-st-c +:`,
+            out: `border-inline-start-color:`,
+        },
+        {
+            in: `bd-i-st-sty:|bd-i-st-sty +:`,
+            out: `border-inline-start-style:`,
+        },
+        {
+            in: `bd-i-st-w:|bd-i-st-w +:`,
+            out: `border-inline-start-width:`,
+        },
+        {
+            in: `bd-i-sty:|bd-i-sty +:`,
+            out: `border-inline-style:`,
+        },
+        {
+            in: `bd-i-w:|bd-i-w +:`,
+            out: `border-inline-width:`,
+        },
+        {
+            in: `bd-l:|bd-l +:`,
+            out: `border-left:`,
+        },
+        {
+            in: `bd-l-c:|bd-l-c +:`,
+            out: `border-left-color:`,
+        },
+        {
+            in: `bd-l-s:|bd-l-s +:`,
+            out: `border-left-style:`,
+        },
+        {
+            in: `bd-l-w:|bd-l-w +:`,
+            out: `border-left-width:`,
+        },
+        {
+            in: `bd-rd:|bd-rd +:`,
+            out: `border-radius:`,
+        },
+        {
+            in: `bd-rg:|bd-rg +:`,
+            out: `border-right:`,
+        },
+        {
+            in: `bd-rg-c:|bd-rg-c +:`,
+            out: `border-right-color:`,
+        },
+        {
+            in: `bd-rg-s:|bd-rg-s +:`,
+            out: `border-right-style:`,
+        },
+        {
+            in: `bd-rg-w:|bd-rg-w +:`,
+            out: `border-right-width:`,
+        },
+        {
+            in: `bd-sp:|bd-sp +:`,
+            out: `border-spacing:`,
+        },
+        {
+            in: `bd-st-e-rd:|bd-st-e-rd +:`,
+            out: `border-start-end-radius:`,
+        },
+        {
+            in: `bd-st-st-rd:|bd-st-st-rd +:`,
+            out: `border-start-start-radius:`,
+        },
+        {
+            in: `bd-sty:|bd-sty +:`,
+            out: `border-style:`,
+        },
+        {
+            in: `bd-t:|bd-t +:`,
+            out: `border-top:`,
+        },
+        {
+            in: `bd-t-c:|bd-t-c +:`,
+            out: `border-top-color:`,
+        },
+        {
+            in: `bd-t-l-rd:|bd-t-l-rd +:`,
+            out: `border-top-left-radius:`,
+        },
+        {
+            in: `bd-t-r-rd:|bd-t-r-rd +:`,
+            out: `border-top-right-radius:`,
+        },
+        {
+            in: `bd-t-s:|bd-t-s +:`,
+            out: `border-top-style:`,
+        },
+        {
+            in: `bd-t-w:|bd-t-w +:`,
+            out: `border-top-width:`,
+        },
+        {
+            in: `bd-w:|bd-w +:`,
+            out: `border-width:`,
+        },
     ],
     // colors
     color: [
