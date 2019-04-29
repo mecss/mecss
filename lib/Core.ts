fb79c52b77daad7f0e2f93906efb76cb6ec940f9
blyndusk <alexandredelaloydev@gmail.com>
2019-04-29 21:00:11 +0200

    💄 update console logs

--- lib/Core.ts
+++ lib/Core.ts
@@ -7,6 +7,8 @@ require(`dotenv`).config();
 const Data = require(`./Data`);
 
 class Core {
+    private aliasLength: number;
+    private aliasFounded: number;
     private input: string | undefined;
     private output: string | undefined;
     private scssInput: string | undefined;
@@ -14,6 +16,8 @@ class Core {
     private watchState: string;
 
     constructor(watchState: string, watchedFile: string) {
+        this.aliasLength = 0;
+        this.aliasFounded = 0;
         this.input = process.env.CSS_INPUT;
         this.output = process.env.CSS_OUTPUT;
         this.scssInput = process.env.SCSS_MASTER;
@@ -41,8 +45,12 @@ class Core {
                     if (Data.hasOwnProperty(prop)) {
                         const props = Data[prop];
                         props.map((x: { in: RegExp; out: string; }) => {
+                            this.aliasLength++;
                             const reg = new RegExp(x.in, `g`);
-                            data = data.replace(reg, x.out);
+                            data = data.replace(reg, () => {
+                                this.aliasFounded++;
+                                return x.out;
+                            });
                         });
                     }
                 }
@@ -56,10 +64,20 @@ class Core {
             }
         });
     }
+    private getAliasFounded() {
+        return console.log(chalk.hex(`#C76161`).bold(`[mecss]`) +  chalk.hex(`#5fcc9e`).bold(` ${this.aliasFounded}`) + ` CSS aliases founded in` + chalk.hex(`#5fcc9e`).bold(` ${process.env.SRC}`));
+    }
+    private getAliasLength() {
+        return console.log(chalk.hex(`#C79161`).bold(`[db]`) + ` contain` + chalk.hex(`#5fcc9e`).bold(` ${this.aliasLength}`) + ` types of CSS aliases`);
+    }
     private deleteUselessFiles() {
         if (!this.watchState || this.watchState === `change`) fs.unlink(this.input, (err: boolean) => {
             if (err) throw err;
-            else this.watchState ? console.log(chalk.hex(`#cacc5f`).bold(`[watch]`) + chalk.hex(`#5fcc9e`).bold(` ${path.basename(this.watchedFile)}`) +  ` watched & compiled`) : undefined;
+            else {
+                this.getAliasFounded();
+                this.getAliasLength();
+                this.watchState ? console.log(chalk.hex(`#cacc5f`).bold(`[watch]`) + chalk.hex(`#5fcc9e`).bold(` ${path.basename(this.watchedFile)}`) +  ` watched & compiled`) : undefined;
+            }
         });
     }
     public start() {
